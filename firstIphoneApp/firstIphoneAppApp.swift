3763b84776d627af2d03e588bef9b23ba8a40bcb
mrbilby <baileymrb@gmail.com>
2023-03-05 19:02:25 +0000

    Initial Commit

---
+++ firstIphoneApp/firstIphoneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstIphoneAppApp.swift
+//  firstIphoneApp
+//
+//  Created by James Bailey on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct firstIphoneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
